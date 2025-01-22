02c6cdb73d97fcaecb61ce431caf94e65c18ed7c
brgndy25 <szn.wavez@gmail.com>
2025-01-22 18:52:59 +0400

    added the cpi client for Kamino Lending

---
+++ kamino-cpi-client/kamino_lending_interface/src/lib.rs
@@ -0,0 +1,9 @@
+solana_program::declare_id!("KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD");
+pub mod accounts;
+pub use accounts::*;
+pub mod typedefs;
+pub use typedefs::*;
+pub mod instructions;
+pub use instructions::*;
+pub mod errors;
+pub use errors::*;
